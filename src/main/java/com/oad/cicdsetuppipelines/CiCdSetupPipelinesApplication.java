1fbd2dcd521dd22978568b18183e363280fe504f
GK_NAIDU <gopiamaradhi1995@gmail.com>
2026-06-11 16:24:44 +0530

    first commit

---
+++ src/main/java/com/oad/cicdsetuppipelines/CiCdSetupPipelinesApplication.java
@@ -0,0 +1,13 @@
+package com.oad.cicdsetuppipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSetupPipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSetupPipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oad/cicdsetuppipelines/CiCdSetupPipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oad.cicdsetuppipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdSetupPipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
